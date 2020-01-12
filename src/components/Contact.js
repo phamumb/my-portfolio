2cc717767a84c3084075b224a47a046355504719
phamumb <48619631+phamumb@users.noreply.github.com>
2020-01-12 10:19:16 -0500

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ export default function Contact() {
             <div class="container-fluid">
                 <p class="text-muted">Although I'm not currently looking for freelance opportunities, my inbox is always open.
                  Whether for a potential project or just to say hi, I'll try my best to answer your email!</p>
-                <button type="button" class="btn btn-outline-light text-center">Say Hi!</button>
+                <button type="button" class="btn btn-outline-light text-center"><a href="mailto:phamdluat@gmail.com"/>Say Hi!</button>
             </div>
         </section>
 
