243b28e2b7dc836932723e8773a3f416177abef9
phamumb <48619631+phamumb@users.noreply.github.com>
2020-01-14 10:18:55 -0500

    edit config

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,13 +15,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/static`,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
