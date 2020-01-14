1daa3071227ea57a51866a6d02a0f1cadc1ce60e
phamumb <48619631+phamumb@users.noreply.github.com>
2020-01-14 08:49:27 -0500

    changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,11 +2,14 @@ import React from 'react'
 export default function Contact() {
     return (
         <section id="contact">
-        <h2 className="text-primary font-weight-bold">Get In Touch</h2>
+            <h2 className="text-primary font-weight-bold">Get In Touch</h2>
             <div class="container-fluid">
                 <p class="text-muted">Although I'm not currently looking for freelance opportunities, my inbox is always open.
                  Whether for a potential project or just to say hi, I'll try my best to answer your email!</p>
-                <button type="button" class="btn btn-outline-light text-center">Say Hi!</button>
+                <form action="mailto:youraddr@domain.tld" method="GET">
+                    <button type="submit" class="btn btn-outline-light text-center">Say Hi!</button>
+                </form>
+
             </div>
         </section>
 
